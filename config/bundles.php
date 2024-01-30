af52919a992940266ade7d57da5749ad08c78174
Dominik Chrastecky <dominik@chrastecky.cz>
2024-01-30 21:56:23 +0100

    Add cors

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Rikudou\JsonApiBundle\RikudouJsonApiBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
