1844585040892bfb358a007da787380e42ea2c5c
Dominik Chrastecky <dominik@chrastecky.cz>
2024-02-09 18:17:37 +0100

    Add debugging component

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,8 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Rikudou\JsonApiBundle\RikudouJsonApiBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
