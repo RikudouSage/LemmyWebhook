ccd6e8fb082393cf6b590e777c0fa7c5b4330528
surprised pikachu <178080424+a-surprised-pikachu@users.noreply.github.com>
2024-11-19 21:35:55 +0100

    Run container as unprivileged user (#26)

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -17,16 +17,13 @@ cd /var/www/html || exit 1
 rm -rf var
 php bin/console cache:clear
 php bin/console cache:warmup
-chown -R 33:33 var
 
 [ ! -d "$DATABASE_DIR" ] && mkdir -p "$DATABASE_DIR"
-chown -R 33:33 "$DATABASE_DIR"
 php bin/console doctrine:migrations:migrate -n
-chmod -R 0777 "$DATABASE_DIR"
 
 php bin/console app:manage-objects --install
 
-/etc/init.d/supervisor start
+/usr/bin/supervisord
 supervisorctl reread
 supervisorctl update
 supervisorctl start messenger-consume:*
